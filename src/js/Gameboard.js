48d17f66cdc41492b397f40e86245404a2fd5731
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-06-27 15:49:27 -0600

    Create empty Gameboard

---
+++ src/js/Gameboard.js
@@ -0,0 +1,12 @@
+export default function createGameboard() {
+	const gameboard = {};
+	gameboard.board = [];
+	for (let i = 0; i < 10; i += 1) {
+		gameboard.board[i] = [];
+		for (let j = 0; j < 10; j += 1) {
+			gameboard.board[i][j] = null;
+		}
+	}
+
+	return gameboard;
+}
