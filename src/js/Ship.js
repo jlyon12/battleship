a59ffbcf04e0c9a92b0bc243ed9d37f15379b976
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-06-26 18:52:59 -0600

    Pass hit, sunk tests for Ship

--- src/js/Ship.js
+++ src/js/Ship.js
@@ -1,12 +1,28 @@
 export default function createShip(length) {
+	const ship = {};
+
 	if (typeof length !== 'number') {
 		throw new TypeError('Battleship length must be a number');
 	}
 	if (!(length >= 2 && length <= 5)) {
 		throw new RangeError('Battleships must be 2, 3, 4, or 5 length');
 	}
+	ship.length = length;
+	ship.hitsTaken = 0;
+	ship.isSunk = false;
 
-	return {
-		length,
+	ship.hit = () => {
+		if (!ship.isSunk) {
+			ship.hitsTaken += 1;
+		}
+		ship.checkIfSunk();
 	};
+
+	ship.checkIfSunk = () => {
+		if (ship.hitsTaken === ship.length) {
+			ship.isSunk = true;
+		}
+	};
+
+	return ship;
 }
