44e39c475e42e6fe0645d847cbb1916a81e79081
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-06-28 11:57:34 -0600

    Write tests for receiving attacks on board

--- src/js/tests/Gameboard.test.js
+++ src/js/tests/Gameboard.test.js
@@ -53,3 +53,27 @@ test('Throw Error if ship placement collides with already placed ship', () => {
 		gameboard.placeShip(3, [2, 0], false);
 	}).toThrow(Error);
 });
+
+test("Successful attacks increase hit ship's damage count", () => {
+	const gameboard = createGameboard();
+	const battleship = gameboard.placeShip(5, [0, 0]);
+	gameboard.receiveAttack([0, 0]);
+	expect(battleship.damage).toBe(1);
+});
+test("Missed attacks do not affect ship's damage count", () => {
+	const gameboard = createGameboard();
+	const battleship = gameboard.placeShip(5, [0, 0]);
+	gameboard.receiveAttack([1, 1]);
+	expect(battleship.damage).toBe(0);
+});
+test('Ships are considered sunk when enough successful hits have occurred', () => {
+	const gameboard = createGameboard();
+	const battleship = gameboard.placeShip(5, [0, 0]);
+	gameboard.receiveAttack([0, 0]);
+	gameboard.receiveAttack([1, 0]);
+	gameboard.receiveAttack([2, 0]);
+	gameboard.receiveAttack([3, 0]);
+	gameboard.receiveAttack([4, 0]);
+	expect(battleship.damage).toBe(5);
+	expect(battleship.sunk).toBe(true);
+});
