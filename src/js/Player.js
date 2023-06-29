51331bf6449fe78cbf8bb5218691e0ebbeeca3f4
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-06-28 18:57:30 -0600

    Add method for player to attack opponent

--- src/js/Player.js
+++ src/js/Player.js
@@ -1,7 +1,17 @@
-export default function createPlayer(name) {
+export default function createHumanPlayer(name) {
 	const player = {};
 	if (typeof name !== 'string')
 		throw new TypeError('Player name must be a string');
 	player.name = name;
+	player.attack = (attackCoords, opponentBoard) => {
+		const target = opponentBoard;
+		if (target.missedAttacks.has(String(attackCoords))) {
+			throw new Error('This attack has already been attempted. It was a miss.');
+		}
+		if (target.successfulAttacks.has(String(attackCoords))) {
+			throw new Error('This attack has already been attempted. It was a hit.');
+		}
+		target.receiveAttack(attackCoords);
+	};
 	return player;
 }
--- src/js/tests/Player.test.js
+++ src/js/tests/Player.test.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 import createPlayer from '../Player';
+import createGameboard from '../Gameboard';
 
 test(`Players can be created with a name`, () => {
 	const player1 = createPlayer('Bob Ross');
@@ -9,4 +10,31 @@ test(`Throw if name !== string`, () => {
 	expect(() => {
 		createPlayer(666);
 	}).toThrow(TypeError);
+});
+test(`Player can attack opponent's board`, () => {
+	const player1 = createPlayer('Bob Ross');
+	const player2 = createPlayer('Robert Ross');
+	const player1Board = createGameboard();
+	const player2Board = createGameboard();
+	player1.attack([0, 0], player2Board);
+	player2.attack([1, 1], player1Board);
+	expect(player2Board.board[0][0]).toBe('M');
+	expect(player1Board.board[1][1]).toBe('M');
+});
+test(`Throw is player has already missed same coords`, () => {
+	const player1 = createPlayer('Bob Ross');
+	const player2Board = createGameboard();
+	player1.attack([0, 0], player2Board);
+	expect(() => {
+		player1.attack([0, 0], player2Board);
+	}).toThrow(Error('This attack has already been attempted. It was a miss.'));
+});
+test(`Throw is player has already success hit same coords`, () => {
+	const player1 = createPlayer('Bob Ross');
+	const player2Board = createGameboard();
+	player2Board.placeShip(3, [0, 0]);
+	player1.attack([0, 0], player2Board);
+	expect(() => {
+		player1.attack([0, 0], player2Board);
+	}).toThrow(Error('This attack has already been attempted. It was a hit.'));
 });
